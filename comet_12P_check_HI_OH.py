3a61cda316aa62a97eb6b0d24d0357c7a02e3bfb
Long-Fei Chen <longf.chen@outlook.com>
2024-05-17 09:59:32 +0800

    clean code

--- comet_12P_check_HI_OH.py
+++ comet_12P_check_HI_OH.py
@@ -4,10 +4,6 @@ import sys
 if sys.version_info.major < 3:
     print("This Code runs under Python 3.")
     exit(0)
-
-
-import os
-import glob
 import numpy as np
 from astropy.time import Time
 import astropy.units as u
@@ -20,12 +16,11 @@ from uwb_tool import pipeline
 
 
 obssetup = {}
-obssetup["ra"] = 233.738433
-obssetup["dec"] = 23.503225
-obssetup["mjd"] = 59906.17638888889
+obssetup["ra"] = None
+obssetup["dec"] = None
+obssetup["mjd"] = None
 obssetup["receiver"] = "UWB2"
 obssetup["bandw"] = functions.UWB_receiver[obssetup["receiver"]]["band_width"]
-obssetup["nchan"] = 1048576
 
 obssetup["sampling_time"] = 1
 obssetup["period_on"] = 1
@@ -37,15 +32,15 @@ obssetup["tcal_data"] = "./tcal/20220617-high-cal/0.95-1.75GHz-Th.xlsx"
 comet_name = "12P"
 obsdate = "20240510"
 
-base_output_path = f"./comet_outputs/{comet_name}/{obsdate}"
+base_data_path = f"./comet_outputs/{comet_name}/{obsdate}"
 
 
 xmin, xmax = 1418, 1422
 freq_mask, freq_array = functions.get_freq_mask_range(obssetup["receiver"], xmin=xmin, xmax=xmax)
 
-HI_on = np.load(f"{base_output_path}/product/sourceON/Ta_{xmin}_{xmax}_ave.npy")
-HI_off = np.load(f"{base_output_path}/product/sourceOFF/Ta_{xmin}_{xmax}_ave.npy")
-HI = np.load(f"{base_output_path}/product/Ta_{xmin}_{xmax}_ave.npy")
+HI_on = np.load(f"{base_data_path}/product/sourceON/Ta_{xmin}_{xmax}_ave.npy")
+HI_off = np.load(f"{base_data_path}/product/sourceOFF/Ta_{xmin}_{xmax}_ave.npy")
+HI = np.load(f"{base_data_path}/product/Ta_{xmin}_{xmax}_ave.npy")
 
 
 plt.plot(freq_array, HI)
@@ -56,12 +51,12 @@ plt.grid()
 plt.show()
 
 
-xmin, xmax = 1656, 1668
+xmin, xmax = 1655, 1670
 freq_mask, freq_array = functions.get_freq_mask_range(obssetup["receiver"], xmin=xmin, xmax=xmax)
 
-OH_on = np.load(f"{base_output_path}/product/sourceON/Ta_{xmin}_{xmax}_ave.npy")
-OH_off = np.load(f"{base_output_path}/product/sourceOFF/Ta_{xmin}_{xmax}_ave.npy")
-OH = np.load(f"{base_output_path}/product/Ta_{xmin}_{xmax}_ave.npy")
+OH_on = np.load(f"{base_data_path}/product/sourceON/Ta_{xmin}_{xmax}_ave.npy")
+OH_off = np.load(f"{base_data_path}/product/sourceOFF/Ta_{xmin}_{xmax}_ave.npy")
+OH = np.load(f"{base_data_path}/product/Ta_{xmin}_{xmax}_ave.npy")
 
 plt.plot(freq_array, OH)
 
--- comet_cal.py
+++ comet_cal.py
@@ -4,76 +4,63 @@ import sys
 if sys.version_info.major < 3:
     print("This Code runs under Python 3.")
     exit(0)
-
-
 import os
-import numpy as np
+from itertools import product
 from uwb_tool import pipeline
 from uwb_tool import functions
 
 
-obssetup = {}
-obssetup["ra"] = None
-obssetup["dec"] = None
-obssetup["receiver"] = "UWB2"
-obssetup["bandw"] = functions.UWB_receiver[obssetup["receiver"]]["band_width"]
-obssetup["nchan"] = 1048576
+def cal_comet_raw_data(comet_name, obsdate, receiver, freq_limit):
+    obssetup = {}
+    obssetup["receiver"] = receiver
+    obssetup["bandw"] = functions.UWB_receiver[receiver]["band_width"]
 
-obssetup["sampling_time"] = 1
-obssetup["period_on"] = 1
-obssetup["period_off"] = 31
-obssetup["source_on"] = 300
-obssetup["source_switch"] = 300
+    obssetup["sampling_time"] = 1
+    obssetup["period_on"] = 1
+    obssetup["period_off"] = 31
+    obssetup["source_on"] = 300
+    obssetup["source_switch"] = 300
 
-obssetup["tcal_file"] = "./tcal/20220617-high-cal/0.95-1.75GHz-Th.xlsx"
-obssetup["fits_files"] = "arp220_onoff-UWB2_000*.fits"
-
-comet_name = "12P"
-obsdate = "20240510"
-
-#### local setup
-# comet_name = "arp220"
-# base_input_path = "./comet_uwb_obs_data"
-# base_output_path = "./comet_outputs"
-# src_path = "/".join([base_input_path, comet_name])
-# out_path = "/".join([base_output_path, comet_name, obsdate])
-
-#### FAST cluster setup
-base_input_path = f"/data31/N2023_9/Comet_UWB/{obsdate}"
-base_output_path = "./comet_outputs"
-src_path = base_input_path
-out_path = "/".join([base_output_path, comet_name, obsdate])
-
-pol_averaged_path = "/".join([out_path, "pol_averaged"])
-
-source_onoff_split_path = "/".join([out_path, "source_onoff"])
-power_source_on_path = source_onoff_split_path+"/"+"sourceON"
-power_source_off_path = source_onoff_split_path+"/"+"sourceOFF"
-
-tacal_data_path = "/".join([out_path, "tacal"])
-tacal_source_on_path = tacal_data_path+"/"+"sourceON"
-tacal_source_off_path = tacal_data_path+"/"+"sourceOFF"
-
-fig_path = "/".join([out_path, "plots"])
-fig_source_on_path = fig_path + "/" + "sourceON"
-fig_source_off_path = fig_path + "/" + "sourceOFF"
-
-prod_path = "/".join([out_path, "product"])
-prod_source_on_path = prod_path + "/" + "sourceON"
-prod_source_off_path = prod_path + "/" + "sourceOFF"
+    obssetup["tcal_file"] = "./tcal/20220617-high-cal/0.95-1.75GHz-Th.xlsx"
+    obssetup["fits_files"] = f"arp220_onoff-{receiver}_*.fits"
 
 
-if not os.path.exists(prod_source_off_path + "/power_ave.npy"):
-    pipeline.polave_fits(obssetup, src_path, pol_averaged_path, fig_path=fig_path)
+    #### FAST cluster setup
+    # base_input_path = f"/data31/N2023_9/Comet_UWB/{obsdate}"
+    base_input_path = f"/media/longfei/N2023_9/Comet_UWB/{obsdate}"
+    base_output_path = "./comet_outputs"
+    src_path = base_input_path
+    out_path = "/".join([base_output_path, comet_name, obsdate])
 
-    load_path = pol_averaged_path
-    pipeline.comet_split_source_onoff(obssetup, load_path, source_onoff_split_path)
+    pol_averaged_path = "/".join([out_path, "pol_averaged"])
 
-    pipeline.power_ave(power_source_on_path, prod_source_on_path)
-    pipeline.power_ave(power_source_off_path, prod_source_off_path)
+    source_onoff_split_path = "/".join([out_path, "source_onoff"])
+    power_source_on_path = source_onoff_split_path+"/"+"sourceON"
+    power_source_off_path = source_onoff_split_path+"/"+"sourceOFF"
+
+    tacal_data_path = "/".join([out_path, "tacal"])
+    tacal_source_on_path = tacal_data_path+"/"+"sourceON"
+    tacal_source_off_path = tacal_data_path+"/"+"sourceOFF"
+
+    fig_path = "/".join([out_path, "plots"])
+    fig_source_on_path = fig_path + "/" + "sourceON"
+    fig_source_off_path = fig_path + "/" + "sourceOFF"
+
+    prod_path = "/".join([out_path, "product"])
+    prod_source_on_path = prod_path + "/" + "sourceON"
+    prod_source_off_path = prod_path + "/" + "sourceOFF"
 
 
-for freq_limit in [[1418, 1422], [1656, 1668]]:
+    if not os.path.exists(prod_source_off_path + "/power_ave.npy"):
+        pipeline.polave_fits(obssetup, src_path, pol_averaged_path, fig_path=fig_path)
+
+        load_path = pol_averaged_path
+        pipeline.comet_split_source_onoff(obssetup, load_path, source_onoff_split_path)
+
+        pipeline.power_ave(power_source_on_path, prod_source_on_path)
+        pipeline.power_ave(power_source_off_path, prod_source_off_path)
+
+
     pipeline.comet_cal_power_data(obssetup, power_source_on_path, tacal_source_on_path, freqlimit=freq_limit, cycle=10, fig_path=fig_source_on_path)
     pipeline.comet_cal_power_data(obssetup, power_source_off_path, tacal_source_off_path, freqlimit=freq_limit, cycle=10, fig_path=fig_source_off_path)
 
@@ -83,3 +70,14 @@ for freq_limit in [[1418, 1422], [1656, 1668]]:
 
     pipeline.merge_figs(obssetup, fig_source_on_path, prod_path+"/sourceONfigs", freqlimit=freq_limit)
     pipeline.merge_figs(obssetup, fig_source_off_path, prod_path+"/sourceOFFfigs", freqlimit=freq_limit)
+
+
+if __name__ == "__main__":
+    comet_name = "12P"
+    obsdate = ["20240417", "20240424", "20240427", "20240429", "20240510", "20240511", "20240513"]
+    receiver = "UWB2"
+    freq_limit_list = [[1418, 1422], [1655, 1670]]
+
+    for obs_date,freq_limit in product(obsdate, freq_limit_list):
+        cal_comet_raw_data(comet_name, obs_date, receiver, freq_limit)
+    
